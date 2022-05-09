7ec15486b15d167a339bee2990d08200e09a93ed
carlos2049 <carlos.20499@gmail.com>
2022-05-09 16:23:41 -0400

    add react-navigation

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 // import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
 import Saludar from './src/components/Saludar';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>curso react native</Text>
-      <Saludar firstname='jose' lastname={22} />
+      <Saludar firstname='jose' lastname='benavides' />
     </View>
   );
 }
